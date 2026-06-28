b7387d8d8c3a06ae0d4b0bc16641a606f36d0929
jeffdavila <jdavila1@byupathway.edu>
2026-06-28 22:38:28 +0800

    commit

---
+++ scripts/getdate.js
@@ -0,0 +1,27 @@
+const full = document.querySelector("#full");
+const worldfull =
+document.querySelector("#world-full");
+const year = document.querySelector("#year");
+const month = document.querySelector("#month");
+const day = document.querySelector("#day");
+const short = document.querySelector("#short")
+const medium = document.querySelector("#medium")
+
+
+function updateDateTime(){
+const today = new Date();
+
+full.innerHTML = `Last Modification: <span
+class="highlight">${new Intl.DateTimeFormat(
+    "en-US",
+{
+    dateStyle: "short",
+    timeStyle: "medium",
+    hour12: false
+}
+).format(today)}
+</span`;
+ 
+}
+
+setInterval(updateDateTime, 1000);
