ddf8447b2b0216d9496b3fc90a52e0e710eee7fb
jeffdavila <jdavila1@byupathway.edu>
2026-07-28 08:26:51 +0800

    commit

---
+++ scripts/form.js
@@ -0,0 +1,54 @@
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+const select = document.querySelector("#product");
+
+products.forEach(product => {
+    const option = document.createElement("option");
+
+    option.textContent = product.name;
+
+    option.value = product.id;
+
+    select.appendChild(option);
+
+    
+});
+// Current Year
+const currentYear = document.querySelector("#currentyear");
+
+if (currentYear) {
+    currentYear.textContent = new Date().getFullYear();
+}
+
+// Last Modified
+const lastModified = document.querySelector("#lastModified");
+
+if (lastModified) {
+    lastModified.textContent = `Last Modification: ${document.lastModified}`;
+}
+
